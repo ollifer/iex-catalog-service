0b61dca22488958b0f68a3cad83dba154e95d0b3
Oleg Olefir <oleg.olefir@tain.com>
2018-12-17 13:35:40 +0200

    Get rid of Thread.sleep in test in favor of timer task

--- src/test/java/iex/rest/CompanyControllerIT.java
+++ src/test/java/iex/rest/CompanyControllerIT.java
@@ -2,8 +2,6 @@ package iex.rest;
 
 import iex.AbstractContainerizedMongoIT;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.internal.Futures;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +13,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
-import java.util.concurrent.Future;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.CountDownLatch;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -31,29 +31,39 @@ public class CompanyControllerIT extends AbstractContainerizedMongoIT {
     @Test
     public void shouldGet2PricesAfter3SecWittPollingInterval2Sec() throws InterruptedException {
 
-        Thread.sleep(3500);
+        CountDownLatch latch = new CountDownLatch(1);
 
-        ResponseEntity<List<CompanyPricesResponse>> response = restTemplate.exchange(
-                "/catalog/company/price?symbols=aapl&companies=fb&timeFrame=3&chronoUnit=SECONDS",
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<List<CompanyPricesResponse>>() {
-                });
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                ResponseEntity<List<CompanyPricesResponse>> response = restTemplate.exchange(
+                        "/catalog/company/price?symbols=aapl&symbols=fb&timeFrame=3&chronoUnit=SECONDS",
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<List<CompanyPricesResponse>>() {
+                        });
 
-        assertThat(response.getStatusCodeValue()).isEqualTo(200);
-        List<CompanyPricesResponse> companyPricesResponse = response.getBody();
+                assertThat(response.getStatusCodeValue()).isEqualTo(200);
+                List<CompanyPricesResponse> companyPricesResponse = response.getBody();
 
-        log.info("Company prices response:  {}", companyPricesResponse);
+                log.info("Company prices response:  {}", companyPricesResponse);
 
-        assertThat(companyPricesResponse).hasSize(1);
+                assertThat(companyPricesResponse).hasSize(1);
 
-        CompanyPricesResponse companyPrice = companyPricesResponse.get(0);
+                CompanyPricesResponse companyPrice = companyPricesResponse.get(0);
 
-        assertThat(companyPrice.getSymbol()).isEqualTo("AAPL");
-        assertThat(companyPrice.getCompanyName()).isNotBlank();
-        assertThat(companyPrice.getLogoUrl()).isNotBlank();
+                assertThat(companyPrice.getSymbol()).isEqualTo("AAPL");
+                assertThat(companyPrice.getCompanyName()).isNotBlank();
+                assertThat(companyPrice.getLogoUrl()).isNotBlank();
 
-        assertThat(companyPrice.getPrices()).hasSize(2);
+                assertThat(companyPrice.getPrices()).hasSize(2);
+
+                latch.countDown();
+            }
+        }, 3000);
+
+        latch.await();
     }
+
 
 }
